231a8afe5204fcaa7dcbf054647065cca6fbadd5
JuanSantos64 <juansanntos17@gmail.com>
2024-04-06 18:45:28 -0300

    Add tela vendas

---
+++ src/main/java/classes/Cliente.java
@@ -0,0 +1,135 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+//nome, cpf, endereço, telefone, e-mail, sexo, estado civil e data de nascimento deverão estar disponíveis na aplicação.
+
+/**
+ *
+ * @author juan.adsantos
+ */
+public class Cliente {
+
+    private String nome;
+    private String CPF;
+    private String endereco;
+    private String telefone;
+    private String email;
+    private String sexo; // ComboBox
+    private String estadoCivil; // ComboBox
+    private String dataDeNascimento;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the CPF
+     */
+    public String getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    /**
+     * @return the endereco
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * @param endereco the endereco to set
+     */
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the estadoCivil
+     */
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    /**
+     * @param estadoCivil the estadoCivil to set
+     */
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    /**
+     * @return the dataDeNascimento
+     */
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    /**
+     * @param dataDeNascimento the dataDeNascimento to set
+     */
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+}
---
+++ src/main/java/classes/Produto.java
@@ -0,0 +1,151 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author juan.adsantos
+ */
+public class Produto {
+
+
+
+    private String ID;
+    private String tamanho;
+    private String cor; //Combobox
+    private String nomeProduto;
+    private String marca;
+    private String categoria; //Combobox
+    private double valor; 
+    private String formaDePagamento; //Combobox
+    private byte qtdParcelas; // Spinner
+
+    /**
+     * @return the ID
+     */
+    public String getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the tamanho
+     */
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    /**
+     * @param tamanho the tamanho to set
+     */
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    /**
+     * @return the cor
+     */
+    public String getCor() {
+        return cor;
+    }
+
+    /**
+     * @param cor the cor to set
+     */
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    /**
+     * @return the nomeProduto
+     */
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    /**
+     * @param nomeProduto the nomeProduto to set
+     */
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    /**
+     * @return the marca
+     */
+    public String getMarca() {
+        return marca;
+    }
+
+    /**
+     * @param marca the marca to set
+     */
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the formaDePagamento
+     */
+    public String getFormaDePagamento() {
+        return formaDePagamento;
+    }
+
+    /**
+     * @param formaDePagamento the formaDePagamento to set
+     */
+    public void setFormaDePagamento(String formaDePagamento) {
+        this.formaDePagamento = formaDePagamento;
+    }
+
+    /**
+     * @return the qtdParcelas
+     */
+    public byte getQtdParcelas() {
+        return qtdParcelas;
+    }
+
+    /**
+     * @param qtdParcelas the qtdParcelas to set
+     */
+    public void setQtdParcelas(byte qtdParcelas) {
+        this.qtdParcelas = qtdParcelas;
+    }
+
+}
